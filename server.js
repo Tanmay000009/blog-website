cbdbbec9aec773cbb875dbc9102d6d8c55c6db67
Tanmay Vyas <70322519+Tanmay000009@users.noreply.github.com>
2021-05-23 03:16:36 +0530

    updated server.js
    Added PORT from env for Heroku Deploy

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.post("/addContent",function(req,res) {
 });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000,function() {
     console.log("Server started on port 3000");
 });
